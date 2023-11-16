3380cfe2c4a7daf8832593551f4428d1e28c224e
CrowsVeldt <zechy.mahler@gmail.com>
2023-11-16 12:46:08 +0200

    ui: replace Input with PasswordInput in Login.jsx

--- frontend/src/pages/public/Login.jsx
+++ frontend/src/pages/public/Login.jsx
@@ -7,20 +7,19 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-} from '@chakra-ui/react';
-import { useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import axios from '../../api/axios';
-import AuthContext from '../../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
+} from "@chakra-ui/react";
+import { useContext, useState } from "react";
+import { toast } from "react-toastify";
+import axios from "../../api/axios";
+import AuthContext from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import PasswordInput from "../../components/inputs/PasswordInput";
 
 const Login = () => {
   const { user, setUser } = useContext(AuthContext);
-  const [show, setShow] = useState(false);
   const [values, setValues] = useState({
-    user_email: '',
-
-    user_password: '',
+    user_email: "",
+    user_password: "",
   });
   const navigate = useNavigate();
 
@@ -31,30 +30,28 @@ const Login = () => {
     }));
   };
 
-  const handleClick = () => setShow(!show);
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const { user_email, user_password } = values;
 
       const response = await axios.post(
-        '/users/customers/login',
+        "/users/customers/login",
         {
           user_email,
           user_password,
         },
         {
-          headers: { 'Content-Type': 'application/json' },
+          headers: { "Content-Type": "application/json" },
           withCredentials: true,
-        },
+        }
       );
       toast.success(response?.data?.message);
       setUser({
         user: response?.data.user,
         accessToken: response?.data.customerToken,
       });
-      navigate('/');
+      navigate("/");
     } catch (error) {
       console.log(error);
       toast.error(error.response?.data.message);
@@ -62,46 +59,37 @@ const Login = () => {
   };
   return (
     <Box
-      as='form'
+      as="form"
       onSubmit={handleSubmit}
-      minH='65vh'
-      maxW='600px'
-      mx='auto'
+      minH="65vh"
+      maxW="600px"
+      mx="auto"
       py={10}
       px={4}
     >
-      <Heading as='h1' size='xl' mb={5}>
+      <Heading as="h1" size="xl" mb={5}>
         Login
       </Heading>
       <FormControl isRequired mb={4}>
         <FormLabel>Email Address</FormLabel>
         <Input
-          name='user_email'
-          type='text'
-          placeholder='Type in your Email'
+          name="user_email"
+          type="text"
+          placeholder="Type in your Email"
           value={values.user_email}
           onChange={handleChange}
         />
       </FormControl>
       <FormControl isRequired mb={4}>
-        <FormLabel>Password</FormLabel>
-        <InputGroup>
-          <Input
-            name='user_password'
-            type={show ? 'text' : 'password'}
-            placeholder='Type in you password'
-            value={values.user_password}
-            onChange={handleChange}
-          />
-          <InputRightElement width='4.5rem'>
-            <Button h='1.75rem' size='sm' onClick={handleClick}>
-              {show ? 'Hide' : 'Show'}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
+        <PasswordInput
+          name="user_password"
+          placeholder="Password"
+          value={values.user_password}
+          state={handleChange}
+        />
       </FormControl>
 
-      <Button type='submit' colorScheme='teal' size='lg' mb={4}>
+      <Button type="submit" colorScheme="teal" size="lg" mb={4}>
         Login
       </Button>
       {/* text for "have account? -> link to login" */}
