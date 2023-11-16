08e6936be84e268c622c52318266a210ba7832fa
CrowsVeldt <zechy.mahler@gmail.com>
2023-11-16 12:39:45 +0200

    ui: add error element to root path in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,47 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 import {
   createBrowserRouter,
   Route,
   RouterProvider,
   createRoutesFromElements,
-} from 'react-router-dom';
-import Root from './pages/Root';
-import Products, { getAllProducts } from './pages/public/Products/Products';
-import About from './pages/public/About';
-import Contact from './pages/public/Contact';
-import Register from './pages/public/Register';
-import Login from './pages/public/Login';
-import Profile from './pages/private/Profile';
-import RequireAuth from './utils/RequireAuth';
-import AuthContext from './context/AuthContext';
-import AutoLogin from './utils/AutoLogin';
+} from "react-router-dom";
+import Root from "./pages/Root";
+import Products, { getAllProducts } from "./pages/public/Products/Products";
+import About from "./pages/public/About";
+import Contact from "./pages/public/Contact";
+import Register from "./pages/public/Register";
+import Login from "./pages/public/Login";
+import Profile from "./pages/private/Profile";
+import RequireAuth from "./utils/RequireAuth";
+import AuthContext from "./context/AuthContext";
+import AutoLogin from "./utils/AutoLogin";
+import ErrorPage from "./error-page.jsx";
 
 function App() {
   const { user } = useContext(AuthContext);
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path='/' element={<Root />}>
+      <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
         <Route element={<AutoLogin />}>
           <Route index loader={getAllProducts} element={<Products />} />
-          <Route path='about' element={<About />} />
-          <Route path='contact' element={<Contact />} />
-          <Route path='register' element={<Register />} />
-          <Route path='login' element={<Login />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
           <Route element={<RequireAuth user={user} />}>
-            <Route path='profile' element={<Profile />} />
+            <Route path="profile" element={<Profile />} />
           </Route>
         </Route>
-      </Route>,
-    ),
+      </Route>
+    )
   );
 
   return (
-    <div className='App'>
+    <div className="App">
       <RouterProvider router={router} />
     </div>
   );
 }
 
 export default App;
-
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,20 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
-import App from './App.jsx';
-import './index.css';
-import CartProvider from './context/CartContext';
-import { AuthProvider } from './context/AuthContext.jsx';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import App from "./App.jsx";
+import CartProvider from "./context/CartContext";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <ChakraProvider>
-      <CartProvider>
-        <AuthProvider>
-          <ToastContainer />
-          <App />
-        </AuthProvider>
-      </CartProvider>
-    </ChakraProvider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider>
+    <CartProvider>
+      <AuthProvider>
+        <ToastContainer />
+        <App />
+      </AuthProvider>
+    </CartProvider>
+  </ChakraProvider>
 );
-
