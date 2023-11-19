1df46b56dbc6b808c020209bd64c917927126d76
CrowsVeldt <zechy.mahler@gmail.com>
2023-11-19 18:08:49 +0200

    api: remove unused code (views, etc.)from backend

--- backend/app.js
+++ backend/app.js
@@ -15,9 +15,6 @@ const productsRouter = require("./routes/products.routes");
 const refreshRouter = require("./routes/refresh.routes");
 
 const app = express();
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(originCredentials);
 app.use(cors(corsOptions));
@@ -32,21 +29,5 @@ app.use("/users", usersRouter);
 app.use("/categories", categoriesRouter);
 app.use("/products", productsRouter);
 app.use("/refresh", refreshRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  console.log("Error Handler:", err);
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.send({ err });
-});
 
 module.exports = app;
